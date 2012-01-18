e9bcfe1821fe175c8703f1539bac45acff436f14
Claudson Oliveira <claudsonweb@gmail.com>
2012-01-18 00:27:03 -0200

    Updating readme and homepage examples

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 <h1>Simple Annotation </h1>
 
-A small php project that insert semantic on class atributtes using annotations docBlox format (/** */). 
 
+A small php project that insert semantic on classes using annotations.<br />
+Today, many programming languages are using annotations for to manager injection depending and
+inversion of control. In the php's core, we haven't this feature, but some projects like <a href="#">Symphony</a> 
+<a href="#">Doctrine</a> and <a href="#">DocBlox</a> was implement.
+This project (with a filosophy <a href="#">MicroPHP manifesto</a> "where small and simple is better") have a goal of
+be a simple layer for insert annotations in other php small projects.<br /><br /> 
 
 <h2>TODO</h2>
 - Add new semantic rules, for example: @var date('Y-m-d H:i:s') for create um Validator better.
--- examples/index.php
+++ examples/index.php
@@ -4,7 +4,12 @@ require_once 'config.php';
 
 echo <<<'HEAD'
 	<h2>SimpleAnnotation</h2>
-	A small php project that insert semantic on classes using annotations<br /><br /><br />
+	A small php project that insert semantic on classes using annotations.<br />
+	Today, many programming languages are using annotations for to manager injection depending and
+    inversion of control. In the php's core, we haven't this feature, but some projects like <a href="#">Symphony</a> 
+    <a href="#">Doctrine</a> and <a href="#">DocBlox</a> was implement.
+	This project (with a filosophy <a href="#">MicroPHP manifesto</a> "where small and simple is better") have a goal of
+	be a simple layer for insert annotations in other php small projects.<br /><br />     
 HEAD;
 
 
@@ -38,6 +43,7 @@ $foo->setValue(3);
 echo '<br /><br />Right now, we created a Foo object and setting your field $value like a integer<br />';
 highlight_string('
 <?php 
+	use Model as m;
     $foo = new m\Foo();
     $foo->setValue(3); 
 ?>');
