3e9a863bcd73f89ef26a21a7f188631267866426
Claudson Oliveira <claudsonweb@gmail.com>
2012-04-03 23:11:48 -0300

    change autoload

--- examples/config.php
+++ examples/config.php
@@ -9,7 +9,7 @@
 $proj = substr(__DIR__,0,strrpos(__DIR__, '/'));
 $root = substr($proj,0,strrpos($proj, '/'));
 
-set_include_path($proj .'/library'. PATH_SEPARATOR . $proj.'/vendor' .PATH_SEPARATOR. $proj.'/vendor/Respect/library' .PATH_SEPARATOR. get_include_path());
+set_include_path($proj .'/library'. PATH_SEPARATOR . $proj.'/vendor' .PATH_SEPARATOR. $proj.'/vendor/Respect/Validation/library' .PATH_SEPARATOR. get_include_path());
 require_once '../library/SimpleAnnotation/SplClassLoader.php';
 
 $myLoader = new \SplClassLoader();
