816e62d173c40056df15fb42f16273b97da29b69
Claudson Oliveira <claudsonweb@gmail.com>
2012-04-14 15:37:23 -0300

    remove SplClassLoader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,24 @@
 <?php
 
-
+/**
+ * 
+ * setting autoload 
+ * @param string $class_name
+ */
+/*
 $proj = substr(__DIR__,0,strrpos(__DIR__, '/'));
+$root = substr($proj,0,strrpos($proj, '/'));
 
-set_include_path($proj .'/library'. PATH_SEPARATOR . $proj.'/examples/Model' .PATH_SEPARATOR. $proj.'/examples' .PATH_SEPARATOR. $proj.'/vendor/Respect/Validation/library' .PATH_SEPARATOR. get_include_path());
-require_once $proj.'/library/SimpleAnnotation/SplClassLoader.php';
+set_include_path($proj .'/library'. PATH_SEPARATOR . $proj.'/vendor' .PATH_SEPARATOR. $proj.'/vendor/Respect/Validation/library' .PATH_SEPARATOR. get_include_path());
+require_once '../library/SimpleAnnotation/SplClassLoader.php';
 
 $myLoader = new \SplClassLoader();
-$myLoader->register();
+$myLoader->register();*/
+
+if(!file_exists(__DIR__.'/../vendor/.composer/autoload.php')){
+   echo 'You need install composer.';
+   exit;
+}
+
+require_once __DIR__.'/../vendor/.composer/autoload.php';
 
